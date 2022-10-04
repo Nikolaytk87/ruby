a476dff4e6a0d2d954117d0237c404c8255d3326
Nikolay <nikolaytk87@mail.ru>
2022-10-04 03:55:04 -0500

    add(lesson1)rectangunal_triangle

---
+++ lesson_1/rectangular_triangle.rb
@@ -0,0 +1,12 @@
+puts "Enter three sides 'a,b,c' of the triangle(Example: 5,5,15): "
+sides_triangle = gets.chomp.split(",")
+
+a, b, c = sides_triangle.map(&:to_i).sort
+
+if a == b && b == c
+  puts "Triangle equilateral and isosceles"
+elsif a == b || b == c
+  puts "Triangle isosceles"
+elsif (a**2) + (b**2) == c**2
+  puts "Triangle rectangular"
+end
