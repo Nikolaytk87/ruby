001345a255274b3ae049e917100d6e92765f6443
Nikolay <nikolaytk87@mail.ru>
2022-10-04 04:25:37 -0500

    add(lesson1) quadratic_equation

---
+++ lesson_1/quadratic_equation.rb
@@ -0,0 +1,17 @@
+puts "Enter 3 coeficents a,b,c comma-separated(Example: 5,5,15): "
+sides_triangle = gets.chomp.split(",")
+
+a, b, c = sides_triangle.map(&:to_i)
+
+discriminant = (b**2) - (4 * a * c)
+
+if discriminant < 0
+  puts "Discriminant: #{discriminant}, There are no roots of the quadratic equation"
+elsif discriminant > 0
+  x1 = (-b + Math.sqrt(discriminant)) / (2 * a)
+  x2 = (-b - Math.sqrt(discriminant)) / (2 * a)
+  puts "Discriminant: #{discriminant}, root1: #{x1} and root2: #{x2}"
+elsif discriminant == 0
+  x = -b / (2 * a)
+  puts "Discriminant: #{discriminant}, root: #{x}"
+end
