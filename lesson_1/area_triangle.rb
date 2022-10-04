03a2dcfc42f4d163411830ff7b7f1abcc4360355
Nikolay <nikolaytk87@mail.ru>
2022-10-04 02:18:59 -0500

    add(lesson1): area_triangle

---
+++ lesson_1/area_triangle.rb
@@ -0,0 +1,7 @@
+puts "Enter the base of the triangle: "
+a_triangle = gets.chomp.to_i
+puts "Enter the base height of the triangle: "
+h_triangle = gets.chomp.to_i
+area_triangle = 0.5 * a_triangle * h_triangle
+
+puts "Area of triangle: #{area_triangle}"
