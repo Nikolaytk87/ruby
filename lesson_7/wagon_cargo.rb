2884fd1645d05acc5f5ae2816a5ba8f5e10ba07d
Nikolay <nikolaytk87@mail.ru>
2022-10-20 09:41:20 -0500

    Feature(wagon_pass)add volumes functional

--- lesson_7/wagon_cargo.rb
+++ lesson_7/wagon_cargo.rb
@@ -1,8 +1,22 @@
 require_relative "wagon.rb"
 
 class CargoWagon < Wagon
-  def initialize(number)
+  attr_reader :volume, :empty_volume, :occupied_volume
+
+  def initialize(number, volume)
+    @volume = volume
+    @empty_volume = volume
+    @occupied_volume = 0
     @type = :cargo
     super
   end
+
+  def take_volume(volume)
+    self.empty_volume -= volume
+    self.occupied_volume += volume
+  end
+
+  private
+
+  attr_writer :empty_volume, :occupied_volume
 end
