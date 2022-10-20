8a1c29aa95880b83e000973cb8aa5033670a800d
Nikolay <nikolaytk87@mail.ru>
2022-10-20 09:34:51 -0500

    Feature(wagon_pass)add seat functional

--- lesson_7/wagon_pass.rb
+++ lesson_7/wagon_pass.rb
@@ -1,23 +1,22 @@
 require_relative "wagon.rb"
 
 class PassengerWagon < Wagon
-  attr_accessor :seats
-  attr_reader :occupied_seats, :empty_seats
+  attr_reader :occupied_seats, :empty_seats, :seats
 
   def initialize(number, seats)
     @type = :passenger
-    @seats = all_seats
+    @seats = seats
     @occupied_seats = 0
     @empty_seats = seats
     super(number)
   end
 
   def take_seat
-    self.empty_seats - 1
-    self.occupied_seats + 1
+    self.empty_seats -= 1
+    self.occupied_seats += 1
   end
 
-  protected
+  private
 
   attr_writer :occupied_seats, :empty_seats
 end
